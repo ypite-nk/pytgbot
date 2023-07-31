155cd161b4b5ab83bf76b2d389ff48790ebc8f1c
Ypiter <N1kita.Fominn@ya.ru>
2023-07-31 20:34:41 +0800

    TASKLIST 3.8.0
    clear code

--- tg-bot/scr/inline.py
+++ tg-bot/scr/inline.py
@@ -1,6 +1,5 @@
 ﻿# -*- coding: utf-8 -*-
 from telegram import InlineQueryResultPhoto, InlineQueryResultArticle, InputTextMessageContent
-
 from uuid import uuid4
 from prefix import prefix_weather
 from spec import checkban
@@ -9,53 +8,43 @@ linkslist = []
 result = []
 
 def inline_query(update, context):
+    if not update.inline_query.query: return
     global linkslist, result
-
-    if not update.inline_query.query:
-        return
-
     if checkban(update, context):
         result = [InlineQueryResultArticle(id=str(uuid4()), title="You can't use this bot", input_message_content=InputTextMessageContent("Error - ban or not mbt"))]
         update.inline_query.answer(result)
         return
-
+    
     if "-" in update.inline_query.query and "w" not in update.inline_query.query:
         command, value = update.inline_query.query.split("-")
         try:
             for i in open("links/mem_links.txt", "r", encoding="utf-8").readlines(0):
-                if i not in linkslist:
-                    linkslist.append(i)
+                if i not in linkslist: linkslist.append(i)
             int(value)
             if command == "memp":
-                try:
-                    result = [InlineQueryResultPhoto(id=str(uuid4()), photo_url=linkslist[int(value)].replace("\n", ""), thumb_url=linkslist[int(value)].replace("\n", ""))]
-                except IndexError:
-                    result = [InlineQueryResultPhoto(id=str(uuid4()), photo_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp",
-                                                                        thumb_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp")]
+                try: result = [InlineQueryResultPhoto(id=str(uuid4()), photo_url=linkslist[int(value)].replace("\n", ""), thumb_url=linkslist[int(value)].replace("\n", ""))]
+                except IndexError: result = [InlineQueryResultPhoto(id=str(uuid4()), photo_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp", thumb_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp")]
             update.inline_query.answer(result)
-        except ValueError:
-            return
+        except ValueError: return
 
     if "w" in update.inline_query.query.lower() and " " in update.inline_query.query:
         value1, value2 = update.inline_query.query.lower().split(" ")
+
         if value2 == 'w':
-            if value1 is None:
-                return
+            if value1 is None: return
             else:
                 result = prefix_weather(None, None, value1)
                 if result is not None:
                     list_val = []
-                    for i in value1:
-                        list_val.append(i)
+                    for i in value1: list_val.append(i)
                     list_val[0] = list_val[0].upper()
+
                     value1 = ""
-                    for i in list_val:
-                        value1 += i
+                    for i in list_val: value1 += i
                     result = [InlineQueryResultArticle(id=str(uuid4()), title=value1 + " погода",
-                                                   input_message_content=InputTextMessageContent("Город: " + value1 + 
+                                                       input_message_content=InputTextMessageContent("Город: " + value1 + 
                                                                          "\nТемпература: " + str(result[0]) +
                                                                          "\nНебо: " + str(result[1]) +
                                                                          "\nВетер: " + str(result[2]) + " м/с"))]
                     update.inline_query.answer(result)
-                else:
-                    return
+                else: return
