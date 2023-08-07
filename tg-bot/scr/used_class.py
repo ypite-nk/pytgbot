94378e257e8937fab0cc9f6b9a07b85db563aa67
Ypiter <N1kita.Fominn@ya.ru>
2023-08-07 19:25:00 +0800

    TASKLIST 3.9.5
    convert to many special

--- tg-bot/scr/used_class.py
+++ tg-bot/scr/used_class.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-links = open("menu/more/fun/photo/links.txt", "r", encoding="utf-8").readlines(0)
-len_data = len(links)
+linksphoto = open("menu/more/fun/photo/links.txt", "r", encoding="utf-8").readlines(0)
+len_data_photo = len(linksphoto)
 
-class Mem():
+class MemPhoto():
     def __init__(self, uid: int):
         self.id = int(uid)
-        self.link = links[self.id]
+        self.link = linksphoto[self.id]
         with open("menu/more/fun/photo/likecount.txt", "r", encoding="utf-8") as lc:
             self.like_count = lc.readlines(0)[self.id]
         with open("menu/more/fun/photo/dislikecount.txt", "r", encoding="utf-8") as dlc:
@@ -13,15 +13,12 @@ class Mem():
         self.raitng = int(self.like_count) - int(self.dislike_count)
 
     def update(self):
-        with open("menu/more/fun/photo/likecount.txt", "r", encoding="utf-8") as lc:
-            self.like_count = lc.readlines(0)[self.id]
-        with open("menu/more/fun/photo/dislikecount.txt", "r", encoding="utf-8") as dlc:
-            self.dislike_count = dlc.readlines(0)[self.id]
+        with open("menu/more/fun/photo/likecount.txt", "r", encoding="utf-8") as lc: self.like_count = lc.readlines(0)[self.id]
+        with open("menu/more/fun/photo/dislikecount.txt", "r", encoding="utf-8") as dlc: self.dislike_count = dlc.readlines(0)[self.id]
 
     def data(self):
         self.update()
-        self.list = [self.link, self.raitng, self.like_count, self.dislike_count]
-        return self.list
+        return [self.link, self.raitng, self.like_count, self.dislike_count]
 
     def change_raiting(self, like_count, dislike_count):
         self.update()
@@ -29,7 +26,7 @@ class Mem():
             Like = file.readlines()
             Like[self.id] = like_count
         with open("menu/more/fun/photo/likecount.txt", "w",  encoding="utf-8") as file:
-            for i in range(len_data):
+            for i in range(len_data_photo):
                 if Like[i] == like_count: file.write(str(Like[i]) + "\n")
                 else: file.write(str(Like[i]))
 
@@ -37,7 +34,7 @@ class Mem():
             Dislike = file.readlines()
             Dislike[self.id] = dislike_count
         with open("menu/more/fun/photo/dislikecount.txt", "w",  encoding="utf-8") as file:
-            for i in range(len_data):
+            for i in range(len_data_photo):
                 if Dislike[i] == dislike_count: file.write(str(Dislike[i]) + "\n")
                 else: file.write(str(Dislike[i]))
 
