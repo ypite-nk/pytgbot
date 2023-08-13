dcea7b3a8ae48760799496ac1bebc8490bc0ef46
Ypiter <N1kita.Fominn@ya.ru>
2023-08-13 17:48:25 +0800

    TASKLIST 4.5.0
    clear code

--- tg-bot/scr/logg.py
+++ tg-bot/scr/logg.py
@@ -3,4 +3,5 @@ file = open("tmp/info.log", "r+")
 password = open("tmp/log.txt", "r").readlines(0)
 def clear(update, context):
     file.truncate(0)
-    context.bot.send_message(chat_id=update.message.chat_id, text="Logg clear")
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text="Logg clear")
