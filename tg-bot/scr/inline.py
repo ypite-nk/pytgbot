b2b56547fc4ce6255fc10006563eca1db673187a
Ypiter <N1kita.Fominn@ya.ru>
2023-08-13 17:48:58 +0800

    TASKLIST 4.5.0
    add profile and city system(need to change city system)
    delete weather_inline

--- tg-bot/scr/inline.py
+++ tg-bot/scr/inline.py
@@ -1,5 +1,5 @@
 ﻿# -*- coding: utf-8 -*-
-from telegram import InlineQueryResultPhoto, InlineQueryResultArticle, InputTextMessageContent
+from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineQueryResultCachedPhoto
 from spec import check_acces
 
 linkslist = []
@@ -12,9 +12,35 @@ def inline_query(update, context):
     global linkslist, result
     
     from uuid import uuid4
+
+    if "profile" in update.inline_query.query.lower() or "профиль" in update.inline_query.query.lower():
+        from prefix import inline_profile
+
+        uid = update.inline_query.from_user.id
+
+        result = [InlineQueryResultArticle(id=str(uuid4()),
+                                           title=f"Профиль пользователя:{uid}",
+                                           input_message_content=InputTextMessageContent(inline_profile(update, context))
+                                           )
+                  ]
+
+        update.inline_query.answer(result)
+
+    elif "city" in update.inline_query.query.lower() or "город" in update.inline_query.query.lower():
+        from prefix import inline_city
+
+        uid = update.inline_query.from_user.id
+        city = inline_city(update, context)
+
+        result = [InlineQueryResultArticle(id=str(uuid4()),
+                                           title=f"Город пользователя:{uid}",
+                                           input_message_content=InputTextMessageContent(city[0])
+                                           )
+                  ]
     
-    if "-" in update.inline_query.query and "w" not in update.inline_query.query:
-        
+    elif "-" in update.inline_query.query and "w" not in update.inline_query.query:
+        from telegram import InlineQueryResultPhoto
+
         command, value = update.inline_query.query.split("-")
         try:
             for i in open("links/mem_links.txt", "r", encoding="utf-8").readlines(0):
@@ -22,39 +48,14 @@ def inline_query(update, context):
             int(value)
             
             if command == "memp":
-                try: result = [InlineQueryResultPhoto(id=str(uuid4()), photo_url=linkslist[int(value)].replace("\n", ""), thumb_url=linkslist[int(value)].replace("\n", ""))]
-                except IndexError: result = [InlineQueryResultPhoto(id=str(uuid4()), photo_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp", thumb_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp")]
+                try: result = [InlineQueryResultPhoto(id=str(uuid4()),
+                                                      photo_url=linkslist[int(value)].replace("\n", ""),
+                                                      thumb_url=linkslist[int(value)].replace("\n", ""))
+                               ]
+                except IndexError: result = [InlineQueryResultPhoto(id=str(uuid4()),
+                                                                    photo_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp",
+                                                                    thumb_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp")
+                                             ]
             update.inline_query.answer(result)
 
-        except ValueError: return
-
-    if "w" in update.inline_query.query.lower() and " " in update.inline_query.query:
-
-        value1, value2 = update.inline_query.query.lower().split(" ")
-        
-        from prefix import prefix_weather
-        
-        if value2 == 'w':
-
-            if value1 is None: return
-            else:
-
-                result = prefix_weather(None, None, value1)
-
-                if result is not None:
-                    list_val = []
-
-                    for i in value1: list_val.append(i)
-                    list_val[0] = list_val[0].upper()
-
-                    value1 = ""
-
-                    for i in list_val: value1 += i
-                    result = [InlineQueryResultArticle(id=str(uuid4()), title=value1 + " погода",
-                                                       input_message_content=InputTextMessageContent("Город: " + value1 + 
-                                                                         "\nТемпература: " + str(result[0]) +
-                                                                         "\nНебо: " + str(result[1]) +
-                                                                         "\nВетер: " + str(result[2]) + " м/с"))]
-                    update.inline_query.answer(result)
-
-                else: return
+        except: return
