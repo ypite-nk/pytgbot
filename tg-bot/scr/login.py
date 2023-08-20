2bde6c32fd7e1964781c199d141c98313fadc873
Ypiter <N1kita.Fominn@ya.ru>
2023-08-21 04:42:11 +0800

    TASKLIST 5.0.0
    add economic-system
    add bank

--- tg-bot/scr/login.py
+++ tg-bot/scr/login.py
@@ -1,6 +1,78 @@
 ﻿# -*- coding: utf-8 -*-
 import os.path as path
-import json
+import json, pytz
+import datetime
+
+def users_profile_info() -> list:
+	with open('base/users_profile.txt', 'r', encoding="utf-8") as f:
+		users = f.readlines(0)
+		info = []
+		for i in users: info.append(i.replace("\n", ""))
+		return info
+
+def users_city_info() -> list:
+	with open('base/users_city.txt', 'r', encoding="utf-8") as f:
+		users = f.readlines(0)
+		info = []
+		for i in users: info.append(i.replace("\n", ""))
+		return info
+	
+def cost_rubles() -> dict:
+	with open("base/costs_r.txt", "r", encoding="utf-8") as file:
+		return json.loads("".join(file.readlines(0)))
+	
+def cost_youshk() -> dict:
+	with open("base/costs_y.txt", "r", encoding="utf-8") as file:
+		return json.loads("".join(file.readlines(0)))
+
+def budget_get() -> dict:
+	if not path.exists("base/bank/budget.txt"):
+		with open("base/bank/budget.txt", "r", encoding="utf-8") as file:
+			return json.loads("".join(file.readlines(0)))
+
+def budget_write(type_write: str = None, value: any = None) -> None:
+	with open("base/bank/budget.txt", "r", encoding="utf-8") as file:
+		file = json.loads("".join(file.readlines(0)))
+			
+		if type_write is None:
+			clear_dict = {
+				"spend": 0,
+				"get": 0
+				}
+			with open("base/bank/budget.txt", "w", encoding="utf-8") as f: f.write(json.dumps(clear_dict))
+
+		else:
+			
+			for key in file.keys():
+				if key == type_write: file[key] = int(file[key]) + int(value)
+		
+			with open("base/bank/budget.txt", "w", encoding="utf-8") as budget: budget.write(json.dumps(file))
+
+def actions_update(type_update: str) -> None:
+	new_key = type_update + datetime.datetime.now(pytz.timezone('Asia/Irkutsk')).date() # 2023-08-15
+		
+	keys, values = [], []
+	inflation = 4
+	
+	with open("base/bank/actions.txt", "r", encoding="utf-8") as file: actions = json.loads("".join(file.readlines(0)))
+
+	for key in actions.keys(): keys.append(key)
+	for value in actions.values(): values.append(value)
+	
+	for name, cost in keys, values:
+		budget = budget_get()
+		budget_write()
+		
+		# set cost for action
+		cost = cost*( (1+inflation) / 100 ) * ( int(budget['get']) - int(budget['spend']) / 100 )
+		#
+		# re-write function
+		with open(f"base/bank/{name}_Memory.txt", "r", encoding="utf-8") as file:
+			memory = json.loads("".join(file.readlines(0)))
+			memory[new_key] = cost
+			
+		with open(f"base/bank/{name}_Memory.txt", "w", encoding="utf-8") as file:
+			file.write(json.dumps(memory))
 
 class User():
 	def __init__(self, uid: str):
@@ -18,7 +90,8 @@ class User():
 			"pr_VIP":"VIP",
 			"pr_Rait":"Рейтинг",
 			"pr_money":"Юшки",
-			"pr_Beta-acc":"Бета-доступ"
+			"pr_Beta-acc":"Бета-доступ",
+			"pr_Lifes":"Попытки"
 			}
 
 		self.translate_dict_to_pr = {
@@ -31,7 +104,8 @@ class User():
 			"VIP":"pr_VIP",
 			"Рейтинг":"pr_Rait",
 			"Юшки":"pr_money",
-			"Бета-доступ":"pr_Beta-acc"
+			"Бета-доступ":"pr_Beta-acc",
+			"Попытки":"pr_Lifes"
 			}
 
 		self.translate_ct_to_dict = {
@@ -82,20 +156,21 @@ class User():
 			"pr_Buisness":"None",
 			"pr_city":"None",
 			"pr_VIP":"None",
-			"pr_Rait":"0",
-			"pr_money":"0",
+			"pr_Rait":0,
+			"pr_money":0,
 			"pr_Beta-acc":"None",
-			"ct_ban":"0",
-			"ct_beta":"0",
-			"ct_admin":"0",
-			"st_nickname":"0",
-			"st_name":"0",
-			"st_birthday":"0",
-			"st_buisness":"0",
-			"ch_nickname":"0",
-			"ch_name":"0",
-			"ch_birthday":"0",
-			"ch_buisness":"0"
+			"pr_Lifes":1,
+			"ct_ban":0,
+			"ct_beta":1,
+			"ct_admin":0,
+			"st_nickname":0,
+			"st_name":0,
+			"st_birthday":0,
+			"st_buisness":0,
+			"ch_nickname":0,
+			"ch_name":0,
+			"ch_birthday":0,
+			"ch_buisness":0
 			}
 
 	def authorize(self) -> None:
@@ -113,7 +188,7 @@ class User():
 			with open(self.user_path, "w", encoding="utf-8") as f: f.write(users_uid_str)
 
 	def get_user_profile(self) -> dict:
-		if not path.exists(self.path): self.authorize()
+		if not path.exists(self.path): return None
 		
 		self.dict = {}
 
@@ -127,7 +202,7 @@ class User():
 			return self.dict
 
 	def get_user_control(self) -> dict:
-		if not path.exists(self.path): self.authorize()
+		if not path.exists(self.path): return None
 
 		if path.exists(self.path):
 			self.dict = {}
@@ -137,12 +212,12 @@ class User():
 
 				for i in file.keys():
 					if "ct_" in i:
-						self.dict[self.translate_ct_to_dict[i]] = int(file[i])
+						self.dict[self.translate_ct_to_dict[i]] = file[i]
 
 				return self.dict
 
 	def get_user_status(self) -> dict:
-		if not path.exists(self.path): self.authorize()
+		if not path.exists(self.path): return None
 
 		if path.exists(self.path):
 			self.dict = {}
@@ -152,12 +227,12 @@ class User():
 
 				for i in file.keys():
 					if "st_" in i:
-						self.dict[self.translate_st_to_dict[i]] = int(file[i])
+						self.dict[self.translate_st_to_dict[i]] = file[i]
 
 				return self.dict
 
 	def get_user_change(self) -> dict:
-		if not path.exists(self.path): self.authorize()
+		if not path.exists(self.path): return None
 
 		if path.exists(self.path):
 			self.dict = {}
@@ -167,7 +242,7 @@ class User():
 
 				for i in file.keys():
 					if "ch_" in i:
-						self.dict[self.translate_ch_to_dict[i]] = int(file[i])
+						self.dict[self.translate_ch_to_dict[i]] = file[i]
 
 				return self.dict
 
@@ -175,7 +250,7 @@ class User():
 		if path.exists(self.path):
 			output = {}
 
-			for i in user_profile_dict.keys(): output[self.translate_dict_to_pr[i]] = str(user_profile_dict[i])
+			for i in user_profile_dict.keys(): output[self.translate_dict_to_pr[i]] = user_profile_dict[i]
 			for i in self.get_user_control().keys(): output[self.translate_dict_to_ct[i]] = self.get_user_control()[i]
 			for i in self.get_user_status().keys(): output[self.translate_dict_to_st[i]] = self.get_user_status()[i]
 			for i in self.get_user_change().keys(): output[self.translate_dict_to_ch[i]] = self.get_user_change()[i]
@@ -188,7 +263,7 @@ class User():
 			output = {}
 
 			for i in self.get_user_profile().keys(): output[self.translate_dict_to_pr[i]] = self.get_user_profile()[i]
-			for i in user_control_dict.keys(): output[self.translate_dict_to_ct[i]] = str(user_control_dict[i])
+			for i in user_control_dict.keys(): output[self.translate_dict_to_ct[i]] = user_control_dict[i]
 			for i in self.get_user_status().keys(): output[self.translate_dict_to_st[i]] = self.get_user_status()[i]
 			for i in self.get_user_change().keys(): output[self.translate_dict_to_ch[i]] = self.get_user_change()[i]
 
@@ -201,7 +276,7 @@ class User():
 
 			for i in self.get_user_profile().keys(): output[self.translate_dict_to_pr[i]] = self.get_user_profile()[i]
 			for i in self.get_user_control().keys(): output[self.translate_dict_to_ct[i]] = self.get_user_control()[i]
-			for i in user_status_dict.keys(): output[self.translate_dict_to_st[i]] = str(user_status_dict[i])
+			for i in user_status_dict.keys(): output[self.translate_dict_to_st[i]] = user_status_dict[i]
 			for i in self.get_user_change().keys(): output[self.translate_dict_to_ch[i]] = self.get_user_change()[i]
 
 			with open(self.path, "w", encoding="utf-8") as file:
@@ -214,24 +289,10 @@ class User():
 			for i in self.get_user_profile().keys(): output[self.translate_dict_to_pr[i]] = self.get_user_profile()[i]
 			for i in self.get_user_control().keys(): output[self.translate_dict_to_ct[i]] = self.get_user_control()[i]
 			for i in self.get_user_status().keys(): output[self.translate_dict_to_st[i]] = self.get_user_status()[i]
-			for i in user_change_dict.keys(): output[self.translate_dict_to_ch[i]] = str(user_change_dict[i])
+			for i in user_change_dict.keys(): output[self.translate_dict_to_ch[i]] = user_change_dict[i]
 
 			with open(self.path, "w", encoding="utf-8") as file:
 				file.write(json.dumps(output))
-
-def users_profile_info() -> list:
-	with open('base/users_profile.txt', 'r', encoding="utf-8") as f:
-		users = f.readlines(0)
-		info = []
-		for i in users: info.append(i.replace("\n", ""))
-		return info
-
-def users_city_info() -> list:
-	with open('base/users_city.txt', 'r', encoding="utf-8") as f:
-		users = f.readlines(0)
-		info = []
-		for i in users: info.append(i.replace("\n", ""))
-		return info
 
 class City():
 	def __init__(self, uid: str):
@@ -243,33 +304,33 @@ class City():
 			"pr_cityname":"None",
 			"pr_country":"None",
 			"pr_subject":"None",
-			"pr_create_data":"2023",
-			"pr_size":"1",
-			"pr_people":"0",
+			"pr_create_data":2023,
+			"pr_size":1,
+			"pr_people":0,
 			"pr_mayor":"None",
 			"pr_sign":"None",
 			"pr_flag":"None",
 			"pr_gymn":"None",
 			"pr_history":"None",
-			"st_cityname":"0",
-			"st_sign":"0",
-			"st_flag":"0",
-			"st_gymn":"0",
-			"st_history":"0",
-			"st_mayor":"0",
-			"dt_budget":"70000",
-			"dt_energyhave":"0",
-			"dt_waterhave":"0",
-			"dt_profit":"50000",
-			"dt_expense":"0",
-			"dt_energyexpense":"0",
-			"dt_waterexpense":"0",
-			"ch_cityname":"0",
-			"ch_sign":"0",
-			"ch_flag":"0",
-			"ch_gymn":"0",
-			"ch_history":"0",
-			"ch_mayor":"0"
+			"st_cityname":0,
+			"st_sign":0,
+			"st_flag":0,
+			"st_gymn":0,
+			"st_history":0,
+			"st_mayor":0,
+			"dt_budget":70000,
+			"dt_energyhave":0,
+			"dt_waterhave":0,
+			"dt_profit":50000,
+			"dt_expense":0,
+			"dt_energyexpense":0,
+			"dt_waterexpense":0,
+			"ch_cityname":0,
+			"ch_sign":0,
+			"ch_flag":0,
+			"ch_gymn":0,
+			"ch_history":0,
+			"ch_mayor":0
 			}
 
 		self.translate_pr_to_dict = {
@@ -394,7 +455,7 @@ class City():
 
 			for i in file.keys():
 				if "st_" in i:
-					output[self.translate_st_to_dict[i]] = int(file[i])
+					output[self.translate_st_to_dict[i]] = file[i]
 
 			return output
 
@@ -408,7 +469,7 @@ class City():
 
 			for i in file.keys():
 				if "dt_" in i:
-					output[self.translate_dt_to_dict[i]] = int(file[i])
+					output[self.translate_dt_to_dict[i]] = file[i]
 
 			return output
 
@@ -422,7 +483,7 @@ class City():
 
 			for i in file.keys():
 				if "ch_" in i:
-					output[self.translate_ch_to_dict[i]] = int(file[i])
+					output[self.translate_ch_to_dict[i]] = file[i]
 
 			return output
 
@@ -430,7 +491,7 @@ class City():
 		if path.exists(self.path):
 			output = {}
 
-			for i in city_profile_dict.keys(): output[self.translate_dict_to_pr[i]] = str(city_profile_dict[i])
+			for i in city_profile_dict.keys(): output[self.translate_dict_to_pr[i]] = city_profile_dict[i]
 			for i in self.get_city_status().keys(): output[self.translate_dict_to_st[i]] = self.get_city_status()[i]
 			for i in self.get_city_data().keys(): output[self.translate_dict_to_dt[i]] = self.get_city_data()[i]
 			for i in self.get_city_change().keys(): output[self.translate_dict_to_ch[i]] = self.get_city_change()[i]
@@ -443,7 +504,7 @@ class City():
 			output = {}
 
 			for i in self.get_city_info().keys(): output[self.translate_dict_to_pr[i]] = self.get_city_info()[i]
-			for i in city_status_dict.keys(): output[self.translate_dict_to_st[i]] = str(city_status_dict[i])
+			for i in city_status_dict.keys(): output[self.translate_dict_to_st[i]] = city_status_dict[i]
 			for i in self.get_city_data().keys(): output[self.translate_dict_to_dt[i]] = self.get_city_data()[i]
 			for i in self.get_city_change().keys(): output[self.translate_dict_to_ch[i]] = self.get_city_change()[i]
 
@@ -456,7 +517,7 @@ class City():
 
 			for i in self.get_city_info().keys(): output[self.translate_dict_to_pr[i]] = self.get_city_info()[i]
 			for i in self.get_city_status().keys(): output[self.translate_dict_to_st[i]] = self.get_city_status()[i]
-			for i in city_data_dict.keys(): output[self.translate_dict_to_dt[i]] = str(city_data_dict[i])
+			for i in city_data_dict.keys(): output[self.translate_dict_to_dt[i]] = city_data_dict[i]
 			for i in self.get_city_change().keys(): output[self.translate_dict_to_ch[i]] = self.get_city_change()[i]
 
 			with open(self.path, "w", encoding="utf-8") as file:
@@ -469,7 +530,41 @@ class City():
 			for i in self.get_city_info().keys(): output[self.translate_dict_to_pr[i]] = self.get_city_info()[i]
 			for i in self.get_city_status().keys(): output[self.translate_dict_to_st[i]] = self.get_city_status()[i]
 			for i in self.get_city_data().keys(): output[self.translate_dict_to_dt[i]] = self.get_city_data()[i]
-			for i in city_change_dict.keys(): output[self.translate_dict_to_ch[i]] = str(city_change_dict[i])
+			for i in city_change_dict.keys(): output[self.translate_dict_to_ch[i]] = city_change_dict[i]
 
 			with open(self.path, "w", encoding="utf-8") as file:
 				file.write(json.dumps(output))
+				
+class Bank():
+	def __init__(self, user_identificator: str):
+		self.uid = user_identificator
+		self.path = f'base/bank/{self.uid}.txt'
+		
+		self.profile = User(self.uid).get_user_profile()
+
+		self.bank = {
+			"Подписка":"Без подписки",
+			"test":0,
+			"test_lvl":0,
+			"Balance":self.profile['Юшки'],
+			"JxSpeed":"False",
+			"MacroMotor":"False"
+			}
+		
+	def authorize(self) -> None:
+		if not path.exists(self.path):
+			with open(self.path, "w", encoding="utf-8") as file: file.write(json.dumps(self.bank))
+			
+		else: return None
+				
+	def get_user_bank(self) -> dict:
+		if path.exists(self.path):
+			with open(self.path, "r", encoding="utf-8") as file: return json.loads("".join(file.readlines(0)))
+			
+		else: return None
+		
+	def write_user_bank(self, new_bank_info: dict) -> None:
+		if path.exists(self.path):
+			with open(self.path, "w", encoding="utf-8") as file: file.write(json.dumps(new_bank_info))
+			
+		else: return None
