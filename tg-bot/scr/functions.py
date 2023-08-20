f1da936b4adaa48d6c49958e986a696cd45d8c2a
Ypiter <N1kita.Fominn@ya.ru>
2023-08-21 04:42:30 +0800

    TASKLIST 5.0.0
    clear

--- tg-bot/scr/functions.py
+++ tg-bot/scr/functions.py
@@ -8,6 +8,7 @@ def start_handler(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=openfile("data", "start"),
                              reply_markup=InlineKeyboardMarkup(kb.start_key))
     from login import User
+    
     User(str(update.message.chat_id)).authorize()
 
 @check_acces
@@ -15,22 +16,25 @@ def back_handler(update, context):
     update.message.reply_text(openfile("menu", "menu"),
                               reply_markup=InlineKeyboardMarkup(kb.start_key))
 
-from spec import Echo_Checker
-
 @check_acces
 def echo_handler(update, context):
+    from spec import Echo_Checker
+
     Checker = Echo_Checker(update, context)
+    
     if Checker.echo_check():
         update.message.reply_text(Checker.message,
                                   reply_markup=Checker.reply_markup)
         return
 
     with open("data/echo/hi.txt", "r", encoding="utf-8") as file: file = file.readlines(0)
+    
     hi = True
     for i in file:
         if i.replace("\n", "").lower() in update.message.text.lower() and hi == True:
             update.message.reply_text(openfile("data/echo", "echohi"),
                                       reply_markup=InlineKeyboardMarkup(kb.back))
             hi = False
+
     if hi == True: update.message.reply_text(openfile("data/echo", "echo"),
                                              reply_markup=InlineKeyboardMarkup(kb.back))
