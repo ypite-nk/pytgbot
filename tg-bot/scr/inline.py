b2f37ad3c6ddcea19da4b59ca70a457c65b302f7
Ypiter <N1kita.Fominn@ya.ru>
2023-08-21 04:42:25 +0800

    TASKLIST 5.0.0
    clear inline

--- tg-bot/scr/inline.py
+++ tg-bot/scr/inline.py
@@ -1,15 +1,10 @@
 ﻿# -*- coding: utf-8 -*-
-from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineQueryResultCachedPhoto
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 from spec import check_acces
-
-linkslist = []
-result = []
 
 @check_acces
 def inline_query(update, context):
     if not update.inline_query.query: return
-
-    global linkslist, result
     
     from uuid import uuid4
 
@@ -18,9 +13,12 @@ def inline_query(update, context):
 
         uid = update.inline_query.from_user.id
 
+        profile = inline_profile(update, context)
+        if not profile: return
+
         result = [InlineQueryResultArticle(id=str(uuid4()),
                                            title=f"Профиль пользователя:{uid}",
-                                           input_message_content=InputTextMessageContent(inline_profile(update, context))
+                                           input_message_content=InputTextMessageContent(profile)
                                            )
                   ]
 
@@ -30,17 +28,22 @@ def inline_query(update, context):
         from prefix import inline_city
 
         uid = update.inline_query.from_user.id
+
         city = inline_city(update, context)
+        if not city: return
 
         result = [InlineQueryResultArticle(id=str(uuid4()),
                                            title=f"Город пользователя:{uid}",
-                                           input_message_content=InputTextMessageContent(city[0])
+                                           input_message_content=InputTextMessageContent(city)
                                            )
                   ]
+
+        update.inline_query.answer(result)
     
     elif "-" in update.inline_query.query and "w" not in update.inline_query.query:
         from telegram import InlineQueryResultPhoto
 
+        linkslist = []
         command, value = update.inline_query.query.split("-")
         try:
             for i in open("links/mem_links.txt", "r", encoding="utf-8").readlines(0):
@@ -56,6 +59,6 @@ def inline_query(update, context):
                                                                     photo_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp",
                                                                     thumb_url="https://raw.githubusercontent.com/ypite-nk/Sticker/main/zero.webp")
                                              ]
-            update.inline_query.answer(result)
+                update.inline_query.answer(result)
 
         except: return
