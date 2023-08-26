847f22a6e236266efb19296fc3a9f83cb7c17fbd
Ypiter <N1kita.Fominn@ya.ru>
2023-08-27 03:25:21 +0800

    TASKLIST 6.2.1
    updates for new cc system

--- tg-bot/scr/functions.py
+++ tg-bot/scr/functions.py
@@ -6,15 +6,18 @@ from spec import check_acces, openfile
 @check_acces
 def start_handler(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=openfile("data", "start"),
-                             reply_markup=InlineKeyboardMarkup(kb.start_key))
+                             reply_markup=InlineKeyboardMarkup(kb.start_key)
+                             )
     from login import User
     
     User(str(update.message.chat_id)).authorize()
 
 @check_acces
 def back_handler(update, context):
-    update.message.reply_text(openfile("menu", "menu"),
-                              reply_markup=InlineKeyboardMarkup(kb.start_key))
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=openfile("menu", "menu"),
+                             reply_markup=InlineKeyboardMarkup(kb.start_key)
+                             )
 
 @check_acces
 def echo_handler(update, context):
@@ -23,8 +26,10 @@ def echo_handler(update, context):
     Checker = Echo_Checker(update, context)
     
     if Checker.echo_check():
-        update.message.reply_text(Checker.message,
-                                  reply_markup=Checker.reply_markup)
+        context.bot.send_message(chat_id=update.message.chat_id,
+                                 text=Checker.message,
+                                 reply_markup=Checker.reply_markup
+                                 )
         return
 
     with open("data/echo/hi.txt", "r", encoding="utf-8") as file: file = file.readlines(0)
@@ -32,9 +37,13 @@ def echo_handler(update, context):
     hi = True
     for i in file:
         if i.replace("\n", "").lower() in update.message.text.lower() and hi == True:
-            update.message.reply_text(openfile("data/echo", "echohi"),
-                                      reply_markup=InlineKeyboardMarkup(kb.back))
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                     text=openfile("data/echo", "echohi"),
+                                     reply_markup=InlineKeyboardMarkup(kb.back)
+                                     )
             hi = False
 
-    if hi == True: update.message.reply_text(openfile("data/echo", "echo"),
-                                             reply_markup=InlineKeyboardMarkup(kb.back))
+    if hi == True: context.bot.send_message(chat_id=update.message.chat_id,
+                                            text=openfile("data/echo", "echo"),
+                                            reply_markup=InlineKeyboardMarkup(kb.back)
+                                            )
